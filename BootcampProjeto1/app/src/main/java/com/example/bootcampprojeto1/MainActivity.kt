adf24977449f4e89d51d0e5df6aeb29121523b52
rubens23 <rubens_assis@outlook.com.br>
2022-06-16 11:16:19 -0300

    viewBinding
    criação do viewBinding

--- BootcampProjeto1/app/src/main/java/com/example/bootcampprojeto1/MainActivity.kt
+++ BootcampProjeto1/app/src/main/java/com/example/bootcampprojeto1/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.bootcampprojeto1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bootcampprojeto1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvHello.text = "lalala"
+
+
     }
 }
