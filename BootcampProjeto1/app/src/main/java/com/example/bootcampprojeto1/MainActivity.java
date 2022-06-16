c7dd19e98ae59378445842feee17513318e90d9f
rubens23 <rubens_assis@outlook.com.br>
2022-06-16 12:45:03 -0300

    binding em java
    binding em java

---
+++ BootcampProjeto1/app/src/main/java/com/example/bootcampprojeto1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.bootcampprojeto1;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bootcampprojeto1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
